1a51e895aa1d878af5d4e305e7cdca76a1dbabb5
Ayush-cmd003 <omdaspurkayastha@gmail.com>
2026-03-09 12:02:47 +0530

    Upated vector db to store unique ids

--- services/vector_store.py
+++ services/vector_store.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 import shutil
-import hashlib
+import uuid
 
 from langchain_community.vectorstores import Chroma
 from langchain_core.documents import Document
@@ -39,14 +39,45 @@ def add_documents(vector_store, embeddings, text, filename):
 
     chunks = splitter.split_documents(docs)
 
+    ids = []
+    unique_chunks = []
+
+    for chunk in chunks:
+        chunk_id = str(uuid.uuid4())
+        ids.append(chunk_id)
+        unique_chunks.append(chunk)
+
+    if vector_store is None:
+
+        db = Chroma.from_documents(
+            unique_chunks,
+            embeddings,
+            ids=ids,
+            persist_directory=st.session_state.vector_path
+        )
+
+        db.persist()
+        return db
+
+    vector_store.add_documents(unique_chunks, ids=ids)
+    vector_store.persist()
+    return vector_store
+
+    docs = [Document(page_content=text.lower(), metadata={"source": filename})]
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=CHUNK_SIZE,
+        chunk_overlap=CHUNK_OVERLAP
+    )
+
+    chunks = splitter.split_documents(docs)
+
     # Generate unique IDs for each chunk
     ids = []
     unique_chunks = []
 
     for chunk in chunks:
-        content = chunk.page_content
-        chunk_id = hashlib.md5((filename + content).encode()).hexdigest()
-
+        chunk_id = str(uuid.uuid4())
         ids.append(chunk_id)
         unique_chunks.append(chunk)
 
